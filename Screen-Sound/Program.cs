95ecbb2a0ec11de0611f268aed3d3b970b3c2f73
Tanjiro561 <davy.correia@spassu.com.br>
2025-09-09 09:59:03 -0300

    First Screen - Welcome rsrs

---
+++ Screen-Sound/Program.cs
@@ -0,0 +1,5 @@
+﻿// Screen Sound
+string MensagemDeVindas = "Boas vindas ao Screen Sound!!";
+Console.WriteLine(MensagemDeVindas);
+Console.WriteLine("Test");
+//Padrão Camel Case - Letra maiúscula no começo
