379a57339b5ee5a33c17155b330adb57db28b662
Davy561 <202304144192@alunos.estacio.br>
2025-09-25 01:07:45 -0300

    feat: add menu basic

--- Screen-Sound/Program.cs
+++ Screen-Sound/Program.cs
@@ -21,5 +21,17 @@ void ExibirMensagemDeBoasVindas()
     Console.WriteLine("=====================================");
 }
 ExibirMensagemDeBoasVindas();
+
+void Exibirmenus()
+{
+    
+    Console.WriteLine("1. Cadastras Bandas");
+    Console.WriteLine("2. Listar Bandas");
+    Console.WriteLine("3. Avaliar Bandas");
+    Console.WriteLine("4. Avaliação Média das Bandas");
+}
+Exibirmenus();
+
+
 Console.ReadLine();
 //Padrão Camel Case - Letra maiúscula no começo
