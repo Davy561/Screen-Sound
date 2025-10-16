f2f083a36f9d4faaa3fe22de5263335dfee4cea3
Davy561 <202304144192@alunos.estacio.br>
2025-10-16 01:31:59 -0300

    feat: menu básico com escolha

--- Screen-Sound/Program.cs
+++ Screen-Sound/Program.cs
@@ -22,13 +22,22 @@ void ExibirMensagemDeBoasVindas()
 }
 ExibirMensagemDeBoasVindas();
 
-void Exibirmenus()
+void Exibirmenus() // Isso é um chamada de função
 {
-    
+
     Console.WriteLine("\n1. Cadastras Bandas");
     Console.WriteLine("2. Listar Bandas");
     Console.WriteLine("3. Avaliar Bandas");
     Console.WriteLine("4. Avaliação Média das Bandas");
+
+    Console.Write("\nDigite -1 para sair:");
+    string opcaoescolhida = Console.ReadLine()!; // Obtém a opção escolhida pelo usuário
+    int opcaoescolhidanumerica = int.Parse(opcaoescolhida); // Converte o tipo do valor de string para int
+    if (opcaoescolhidanumerica == 1) // Compara o valor escolhido pelo usuário por o número 1 e devolve o valor escolhido 
+    {
+        Console.WriteLine($"Você escolheu a opção " + opcaoescolhidanumerica);
+    }
+
 }
 Exibirmenus();
 
