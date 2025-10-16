6e6d8c8a1461f00f5a039dde5059d12cfb7ccff3
Davy561 <202304144192@alunos.estacio.br>
2025-10-16 01:52:15 -0300

    feat: adicionada estrutura switch/case para melhoria de estrutura de condições

--- Screen-Sound/Program.cs
+++ Screen-Sound/Program.cs
@@ -33,10 +33,30 @@ void Exibirmenus() // Isso é um chamada de função
     Console.Write("\nDigite -1 para sair:");
     string opcaoescolhida = Console.ReadLine()!; // Obtém a opção escolhida pelo usuário
     int opcaoescolhidanumerica = int.Parse(opcaoescolhida); // Converte o tipo do valor de string para int
-    if (opcaoescolhidanumerica == 1) // Compara o valor escolhido pelo usuário por o número 1 e devolve o valor escolhido 
+    switch (opcaoescolhidanumerica) // Isso serve como estrutura de condições if e else, só que de uma forma mais simples e menos complexa
     {
-        Console.WriteLine($"Você escolheu a opção " + opcaoescolhidanumerica);
+        case 1:
+            Console.WriteLine($"Você escolheu a opção" + opcaoescolhidanumerica);
+        break;
+        case 2:
+            Console.WriteLine($"Você escolheu a opção" + opcaoescolhidanumerica);
+        break;
+        case 3:
+            Console.WriteLine($"Você escolheu a opção" + opcaoescolhidanumerica);
+        break;
+        case 4:
+            Console.WriteLine($"Você escolheu a opção" + opcaoescolhidanumerica);
+        break;
+        case -1:
+            Console.WriteLine($"Você escolheu a opção" + opcaoescolhidanumerica);
+        break;
+
+
+
+
+
     }
+
 
 }
 Exibirmenus();
--- Screen-Sound/obj/Debug/net8.0/Screen-Sound.AssemblyInfo.cs
+++ Screen-Sound/obj/Debug/net8.0/Screen-Sound.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Screen-Sound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44c3d4cfaebe8ef4439ce279839d1f361ff5de36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f083a36f9d4faaa3fe22de5263335dfee4cea3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Screen-Sound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Screen-Sound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
