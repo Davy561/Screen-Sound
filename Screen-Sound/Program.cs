dbe80dba8815d5c2b85997fbb3ea2baed7b56fe9
Davy561 <202304144192@alunos.estacio.br>
2025-10-27 01:30:59 -0300

    Fix menu + refatoração + add insert data band music

--- Screen-Sound/Program.cs
+++ Screen-Sound/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Screen Sound
 string MensagemDeVindas = "Boas vindas ao Screen Sound!!";
-void ExibirMensagemDeBoasVindas()
+List<string> Listarbandas = new List<string>();
+void ExibirLogo()
 {
     Console.WriteLine(@"
      
@@ -20,11 +21,10 @@ void ExibirMensagemDeBoasVindas()
     Console.WriteLine(MensagemDeVindas);
     Console.WriteLine("=====================================");
 }
-ExibirMensagemDeBoasVindas();
 
 void Exibirmenus() // Isso é um chamada de função
 {
-
+    ExibirLogo();// Já chama a logo mais a mensagem de boas vindas dentro do código evitando poluição no ambiente de produção
     Console.WriteLine("\n1. Cadastras Bandas");
     Console.WriteLine("2. Listar Bandas");
     Console.WriteLine("3. Avaliar Bandas");
@@ -35,20 +35,21 @@ void Exibirmenus() // Isso é um chamada de função
     int opcaoescolhidanumerica = int.Parse(opcaoescolhida); // Converte o tipo do valor de string para int
     switch (opcaoescolhidanumerica) // Isso serve como estrutura de condições if e else, só que de uma forma mais simples e menos complexa
     {
-        case 1:
-            Console.WriteLine($"Você escolheu a opção" + opcaoescolhidanumerica);
-        break;
-        case 2:
-            Console.WriteLine($"Você escolheu a opção" + opcaoescolhidanumerica);
+        case 1: RegistrarBanda();
+            Console.WriteLine();
+            
+            break;
+        case 2: MostrarBandas();
+            Console.WriteLine();
         break;
         case 3:
-            Console.WriteLine($"Você escolheu a opção" + opcaoescolhidanumerica);
+            Console.WriteLine($"Você escolheu a opção " + opcaoescolhidanumerica);
         break;
         case 4:
-            Console.WriteLine($"Você escolheu a opção" + opcaoescolhidanumerica);
+            Console.WriteLine($"Você escolheu a opção " + opcaoescolhidanumerica);
         break;
         case -1:
-            Console.WriteLine($"Você escolheu a opção" + opcaoescolhidanumerica);
+            Console.WriteLine($"Você escolheu a opção " + opcaoescolhidanumerica);
         break;
 
 
@@ -58,6 +59,31 @@ void Exibirmenus() // Isso é um chamada de função
     }
 
 
+}
+void RegistrarBanda()
+{
+    Console.Clear();
+    Console.WriteLine("Registrar bandas\n");
+    Console.WriteLine("Qual banda você deseja resgistra hoje ?");
+    string nomedabanda = Console.ReadLine()!;
+    Listarbandas.Add(nomedabanda);
+    Console.Clear();
+    Console.WriteLine($"A banda {nomedabanda} foi registrada com sucesso!!");
+    Thread.Sleep( 2000 );
+    Console.Clear();
+    ExibirLogo();
+    Exibirmenus();
+}
+void MostrarBandas()
+{
+    Console.Clear();
+    for (int i = 0; i < Listarbandas.Count; i++)
+    {
+        Console.WriteLine(Listarbandas[i]);
+    }
+    Thread.Sleep(2000);
+    Console.Clear();
+    Exibirmenus();
 }
 Exibirmenus();
 
